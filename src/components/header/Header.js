0fc9de6c2765048da10886a82fd81928c3ef2652
Alaa Eddine <keni91cell@gmail.com>
2021-04-21 19:44:50 +0200

    adds routes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Styles.module.scss";
 
 const Header = () => {
   return (
     <div className={styles.header_container}>
       <div className={styles.logo_container}>
-        <h1>A.E</h1>
+        <NavLink to="/">
+          <h1>A.E</h1>
+        </NavLink>
       </div>
       <div className={styles.navbar_container}>
         <div className={styles.navlinks_wrapper}>
-          <h1>Home</h1>
-          <h1>About me</h1>
-          <h1>Portfolio</h1>
+          <NavLink to="/about" style={{ textDecoration: "none" }}>
+            <h1>About me</h1>
+          </NavLink>
+          <NavLink to="skills">
+            <h1>Skills</h1>
+          </NavLink>
+          <NavLink to="/portfolio">
+            <h1>Portfolio</h1>
+          </NavLink>
         </div>
       </div>
     </div>
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./Styles.module.scss";
 
 const Skills = () => {
   return (
-    <div>
+    <div className={styles.skills_container}>
       <h1>Skills</h1>
     </div>
   );

ac68bada2e505182cef7706b57f410d916272e73
Alaa Eddine <keni91cell@gmail.com>
2021-04-22 17:49:45 +0200

    adds letters pattern home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,17 +6,33 @@ const Home = () => {
     <div className={styles.home_container}>
       <div className={styles.text_wrapper}>
         <h2>pwhscws4d5wfrfshsw48d5aav3hjk</h2>
-        <h2>rhxHIg4IsAMxcptrhxmsg46saacxc</h2>
-        <h2>jaszsdvjas8sdv6dssdse3dssdse3</h2>
-        <h2>sdah84omlhALAArsEDDINEs5A21sw</h2>
-        <h2>d8wtnerA8wqnvdps83s52ds83s52d</h2>
-        <h2>25ewvks25e2vk3ualvaFULLlvamkl</h2>
-        <h2>wpo7ndswpoSTACKhvkxsMrhvkxswr</h2>
-        <h2>95sMc58lwqycym6oapcmEobmssswz</h2>
-        <h2>uka9zp4ukalzp4te2xeeR1e2xee41</h2>
-        <h2>ptrhxmsptrhxmsy46sacNc46sacxc</h2>
-        <h2>jas8sdv2dssdse3jas8sdv2dssdse</h2>
         <h2>ksv5echekjc2s5vkjuev5a25cveyn</h2>
+        <h2>
+          rhx<span>HI!</span>4<span>I</span>s<span>AM</span>xcptrhxmsg46saacxc
+        </h2>
+        <h2>jaszsdvjas8sdv6dssdse3dssdse3</h2>
+        <h2>
+          sdah84omlh<span>ALAA</span>s<span>EDDINE</span>sr5A21sw
+        </h2>
+        <h2>d8wtnerA8wqnvdp83s52ds83s52d83s52ds83s52d</h2>
+        <h2>
+          25ewvks25e<span>A</span>a<span>FULL</span>2vk3ual8vamkl
+        </h2>
+        <h2>
+          wpo7ndswpohvkxs<span>S</span>Mrhvkxswrpqlx
+        </h2>
+        <h2>
+          uka9zp4ukalzp4e<span>T</span>2xeeR1e2xee41
+        </h2>
+        <h2>
+          95sMc<span>MERN</span>qycym6<span>A</span>a<span>DEVELOPER</span>swz
+        </h2>
+        <h2>
+          jas8sdv2dssdse3<span>C</span>ssdsesgd4hwqk
+        </h2>
+        <h2>
+          ptrhxmsptrhxmsy<span>K</span>sruacc46sacxc
+        </h2>
         <h2>ksv5echekjc2s5vkjuev5a25cveyn</h2>
       </div>
     </div>

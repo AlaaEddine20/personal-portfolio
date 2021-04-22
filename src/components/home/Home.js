3235d0af3848b2a28dcbd7c7dc8889e3c15f0804
Alaa Eddine <keni91cell@gmail.com>
2021-04-22 18:20:12 +0200

    adds hi and mern words animations

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,8 @@ const Home = () => {
         <h2>pwhscws4d5wfrfshsw48d5aav3hjk</h2>
         <h2>ksv5echekjc2s5vkjuev5a25cveyn</h2>
         <h2>
-          rhx<span>HI!</span>4<span>I</span>s<span>AM</span>xcptrhxmsg46saacxc
+          rhx<span className={styles.animated_hi}>HI!</span>4<span>I</span>s
+          <span>AM</span>xcptrhxmsg46saacxc
         </h2>
         <h2>jaszsdvjas8sdv6dssdse3dssdse3</h2>
         <h2>
@@ -19,13 +20,14 @@ const Home = () => {
           25ewvks25e<span>A</span>a<span>FULL</span>2vk3ual8vamkl
         </h2>
         <h2>
-          wpo7ndswpohvkxs<span>S</span>Mrhvkxswrpqlx
+          wpo7ndswpohvkxs<span>S</span>Mrhvkx
+          <span className={styles.animated}>MERN</span>wrpqlx
         </h2>
         <h2>
           uka9zp4ukalzp4e<span>T</span>2xeeR1e2xee41
         </h2>
         <h2>
-          95sMc<span>MERN</span>qycym6<span>A</span>a<span>DEVELOPER</span>swz
+          95sMckqycy3cqm6<span>A</span>a<span>DEVELOPER</span>swz
         </h2>
         <h2>
           jas8sdv2dssdse3<span>C</span>ssdsesgd4hwqk
